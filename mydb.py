2c88385052838d49203bb0804dbaf62308b843f6
thenarrator2 <manasbihani25@gmail.com>
2023-04-05 18:01:19 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,11 @@
+import mysql.connector
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'password123'
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE elderco")
+print('all done')
